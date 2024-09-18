2ca0dc0adb5f484ee4b88b4d0512f7a9ec38e8cf
shiwoo.park <custardcream@kakao.com>
2024-09-19 04:53:53 +0900

    playground feat: add mockServiceWorker

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,14 +1,33 @@
 import { installMSWDevtool } from "@custardcream/msw-devtool"
+import { http, HttpResponse } from "msw"
 import { setupWorker } from "msw/browser"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import App from "./App.tsx"
 
-installMSWDevtool({ api: setupWorker() })
+const enableMocking = async () => {
+  if (import.meta.env.PROD) {
+    return
+  }
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  return installMSWDevtool({
+    api: setupWorker(
+      http.get("https://test-api.co.kr", () => {
+        return HttpResponse.json({
+          id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
+          firstName: "John",
+          lastName: "Maverick"
+        })
+      })
+    )
+  })
+}
+
+enableMocking().then(() =>
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
 )
