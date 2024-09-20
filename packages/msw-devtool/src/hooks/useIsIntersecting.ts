c8b21a9c4805f1e769e02b1dd6a82f8481c09f0d
shiwoo.park <custardcream@kakao.com>
2024-09-20 22:54:23 +0900

    feat: change useIsIntersecting default value

--- packages/msw-devtool/src/hooks/useIsIntersecting.ts
+++ packages/msw-devtool/src/hooks/useIsIntersecting.ts
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { useIntersectionObserver } from "~/hooks/useIntersectionObserver"
 
 export const useIsIntersecting = (target: React.RefObject<Element>) => {
-  const [isIntersecting, setIsIntersecting] = useState(false)
+  const [isIntersecting, setIsIntersecting] = useState(true)
 
   useIntersectionObserver(target, { threshold: 0.5 }, ([entry]) => {
     setIsIntersecting(entry.isIntersecting)
