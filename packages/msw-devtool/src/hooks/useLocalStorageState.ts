e9b55ea28834a25a4c152313cc8e9665f58f0fc7
shiwoo.park <custardcream@kakao.com>
2024-09-21 00:40:49 +0900

    feat: add useLocalStorageState hook

---
+++ packages/msw-devtool/src/hooks/useLocalStorageState.ts
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react"
+
+export const useLocalStorageState = <T extends object | string | number | null>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.error(error)
+      return initialValue
+    }
+  })
+
+  const setValue = useCallback(
+    (value: React.SetStateAction<T>) => {
+      setStoredValue((prevValue) => {
+        const finalValue =
+          typeof value === "function" ? value(prevValue) : value
+
+        window.localStorage.setItem(key, JSON.stringify(finalValue))
+
+        return finalValue
+      })
+    },
+    [key]
+  )
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === key) {
+        setStoredValue(JSON.parse(event.newValue ?? "null"))
+      }
+    }
+
+    window.addEventListener("storage", handleStorageChange)
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange)
+    }
+  }, [key])
+
+  return [storedValue, setValue]
+}
