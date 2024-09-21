1901c4354b04a6fb9612c297dcf178246529626d
shiwoo.park <custardcream@kakao.com>
2024-09-21 16:46:55 +0900

    fix: make msw not to be bundled in prod

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -6,14 +6,12 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 
 const enableMocking = async () => {
-  if (import.meta.env.PROD) {
-    return
+  if (import.meta.env.DEV) {
+    return installMSWDevtool({
+      initialOpen: true,
+      api: setupWorker()
+    })
   }
-
-  return installMSWDevtool({
-    initialOpen: true,
-    api: setupWorker()
-  })
 }
 
 enableMocking().then(() =>
