1295d224ea708c7ba95808e07c7abb82ff51df88
shiwoo.park <custardcream@kakao.com>
2024-09-22 13:44:21 +0900

    feat: add useBoolean hook

---
+++ packages/msw-devtools/src/hooks/useBoolean.ts
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react"
+
+export const useBoolean = (initialValue = false) => {
+  const [value, setValue] = useState(initialValue)
+
+  const setTrue = useCallback(() => setValue(true), [])
+  const setFalse = useCallback(() => setValue(false), [])
+  const toggle = useCallback(() => setValue((prevValue) => !prevValue), [])
+
+  return [value, setTrue, setFalse, toggle]
+}
