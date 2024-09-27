dbb325f34e5edd619bd76302a075e814b989de6b
shiwoo.park <custardcream@kakao.com>
2024-09-27 20:28:43 +0900

    feat: remove dimmer

--- packages/msw-devtools/src/components/ScrollList.tsx
+++ packages/msw-devtools/src/components/ScrollList.tsx
@@ -1,7 +1,4 @@
 import { clsx } from "clsx"
-import { useRef } from "react"
-
-import { useIsIntersecting } from "~/hooks/useIsIntersecting"
 
 export const ScrollList = ({
   className,
@@ -9,22 +6,9 @@ export const ScrollList = ({
 }: React.PropsWithChildren<{
   className?: string
 }>) => {
-  const intersectionSensorRef = useRef<HTMLDivElement>(null)
-  const isScrolled = !useIsIntersecting(intersectionSensorRef)
-
   return (
-    <div
-      className={clsx(
-        className,
-        "relative h-full min-h-0",
-        "after:absolute after:left-0 after:right-0 after:top-0 after:h-5 after:bg-gradient-to-b after:from-gray-300 after:bg-[length:100%_20px] after:bg-left-top after:bg-no-repeat after:transition-opacity after:content-['']",
-        isScrolled ? "after:opacity-1" : "after:opacity-0"
-      )}
-    >
-      <div className='h-full overflow-auto p-3'>
-        <div ref={intersectionSensorRef}></div>
-        {children}
-      </div>
+    <div className={clsx(className, "relative h-full min-h-0")}>
+      <div className='h-full overflow-auto p-3'>{children}</div>
     </div>
   )
 }
