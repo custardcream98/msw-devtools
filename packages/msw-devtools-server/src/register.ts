eb37fb88c4892713ca806fa72b5184c26e2c0e91
shiwoo.park <custardcream@kakao.com>
2024-10-13 06:46:27 +0900

    feat: use chokidar instead of ws

--- packages/msw-devtools-server/src/register.ts
+++ packages/msw-devtools-server/src/register.ts
@@ -1,66 +1,12 @@
-import {
-  eventGuard,
-  generateHandler,
-  type JsonMock,
-  MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG,
-  MSWDevtoolsWebsocketEventName,
-  serializeMSWDevtoolsWebsocketEvent
-} from "core"
+import { generateHandler } from "core"
 import type { HttpHandler } from "msw"
 import type { SetupServerApi } from "msw/node"
-import { type RawData, WebSocket } from "ws"
 
-import { readMockListFile } from "~/file"
-
-const ws = new WebSocket(
-  `ws://localhost:${MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG.PORT}${MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG.PATH}`
-)
+import { readMockListFile, watchMockListFile } from "~/file"
 
 export const register = ({ server }: { server: SetupServerApi }) => {
-  ws.on("open", () => {
-    const use = (jsonMocks: JsonMock[]) => {
-      server.resetHandlers(...jsonMocks.map(generateHandler))
-    }
-
-    ws.send(
-      serializeMSWDevtoolsWebsocketEvent({
-        name: MSWDevtoolsWebsocketEventName.SYN
-      })
-    )
-
-    const ackListener = (data: RawData) => {
-      eventGuard(
-        data.toString(),
-        MSWDevtoolsWebsocketEventName.ACK,
-        (initialMockList) => {
-          ws.off("message", ackListener)
-          if (initialMockList) {
-            use(initialMockList)
-          }
-
-          ws.send(
-            serializeMSWDevtoolsWebsocketEvent({
-              name: MSWDevtoolsWebsocketEventName.ACK,
-              payload: null
-            })
-          )
-
-          const mockListUpdateListener = (data: RawData) => {
-            eventGuard(
-              data.toString(),
-              MSWDevtoolsWebsocketEventName.MOCK_LIST_UPDATE,
-              (jsonMocks) => {
-                use(jsonMocks)
-              }
-            )
-          }
-
-          ws.on("message", mockListUpdateListener)
-        }
-      )
-    }
-
-    ws.on("message", ackListener)
+  watchMockListFile("mockList.json", (mockList) => {
+    server.resetHandlers(...mockList.map(generateHandler))
   })
 }
 
