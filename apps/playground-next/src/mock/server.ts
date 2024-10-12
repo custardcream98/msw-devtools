7683b8bb5206e39e4ad25f8f4908649faebb1204
shiwoo.park <custardcream@kakao.com>
2024-10-13 06:37:20 +0900

    feat: server side handler hmr

--- apps/playground-next/src/mock/server.ts
+++ apps/playground-next/src/mock/server.ts
@@ -1,8 +1,8 @@
-import { jsonMocksToHandlers } from "@custardcream/msw-devtools-server"
+import { getInitialHandlers, register } from "@custardcream/msw-devtools-server"
 import { setupServer } from "msw/node"
 
-import mocks from "../../mockList.json"
-
-const server = setupServer(...jsonMocksToHandlers(mocks))
+const server = setupServer(...getInitialHandlers())
 
 server.listen()
+
+register({ server })
---
+++ packages/msw-devtools-server/src/register.ts
@@ -0,0 +1,69 @@
+import {
+  eventGuard,
+  generateHandler,
+  type JsonMock,
+  MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG,
+  MSWDevtoolsWebsocketEventName,
+  serializeMSWDevtoolsWebsocketEvent
+} from "core"
+import type { SetupServerApi } from "msw/node"
+import { type RawData, WebSocket } from "ws"
+
+import { readMockListFile } from "~/file"
+import { jsonMocksToHandlers } from "~/jsonMockToHandlers"
+
+const ws = new WebSocket(
+  `ws://localhost:${MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG.PORT}${MSW_DEVTOOLS_WEBSOCKET_SERVER_CONFIG.PATH}`
+)
+
+export const register = ({ server }: { server: SetupServerApi }) => {
+  ws.on("open", () => {
+    const use = (jsonMocks: JsonMock[]) => {
+      server.resetHandlers(...jsonMocks.map(generateHandler))
+    }
+
+    ws.send(
+      serializeMSWDevtoolsWebsocketEvent({
+        name: MSWDevtoolsWebsocketEventName.SYN
+      })
+    )
+
+    const ackListener = (data: RawData) => {
+      eventGuard(
+        data.toString(),
+        MSWDevtoolsWebsocketEventName.ACK,
+        (initialMockList) => {
+          ws.off("message", ackListener)
+          if (initialMockList) {
+            use(initialMockList)
+          }
+
+          ws.send(
+            serializeMSWDevtoolsWebsocketEvent({
+              name: MSWDevtoolsWebsocketEventName.ACK,
+              payload: null
+            })
+          )
+
+          const mockListUpdateListener = (data: RawData) => {
+            eventGuard(
+              data.toString(),
+              MSWDevtoolsWebsocketEventName.MOCK_LIST_UPDATE,
+              (jsonMocks) => {
+                use(jsonMocks)
+              }
+            )
+          }
+
+          ws.on("message", mockListUpdateListener)
+        }
+      )
+    }
+
+    ws.on("message", ackListener)
+  })
+}
+
+export const getInitialHandlers = () => {
+  return jsonMocksToHandlers(readMockListFile("mockList.json"))
+}
