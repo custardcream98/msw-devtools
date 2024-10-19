a8c62140048edbab436cb79c45bcd8dd8876b243
shiwoo.park <custardcream@kakao.com>
2024-10-20 00:41:15 +0900

    feat: remove msw native support

--- packages/msw-devtools/src/lib/msw/initialize.ts
+++ packages/msw-devtools/src/lib/msw/initialize.ts
@@ -1,6 +1,4 @@
-import type { SharedOptions } from "msw"
 import type { SetupWorker, StartOptions } from "msw/browser"
-import type { setupServer as setupServerNative } from "msw/native"
 
 import { StorageKey } from "~/constants"
 import { getLocalStorageItem } from "~/hooks/useLocalStorageState"
@@ -9,33 +7,21 @@ import { autoFixJsonMock } from "~/utils/autoFixJsonMock"
 import { register } from "./register"
 import { getWorkerWithoutThrow, setWorker } from "./worker"
 
-export type InitializeProps =
-  | {
-      setupWorker: SetupWorker
-      /**
-       * @link https://mswjs.io/docs/api/setup-worker/start/#options
-       */
-      options?: StartOptions
-    }
-  | {
-      setupWorker: ReturnType<typeof setupServerNative>
-      /**
-       * @link https://mswjs.io/docs/api/setup-server/listen/#options
-       */
-      options?: Partial<SharedOptions>
-    }
+export type InitializeProps = {
+  setupWorker: SetupWorker
+  /**
+   * @link https://mswjs.io/docs/api/setup-worker/start/#options
+   */
+  options?: StartOptions
+}
+
 export const initialize = async ({ setupWorker, options }: InitializeProps) => {
   if (getWorkerWithoutThrow()) {
     throw new Error("[MSW Devtools] Devtool already initialized")
   }
 
   setWorker(setupWorker)
-
-  if ("start" in setupWorker) {
-    await setupWorker.start(options)
-  } else {
-    setupWorker.listen(options)
-  }
+  await setupWorker.start(options)
 
   const localStorageMocks = getLocalStorageItem(StorageKey.MOCK_LIST)
     ?.map(autoFixJsonMock)
