464005ad74bdd66c1ddf41957c3eeffa48dc112e
shiwoo.park <custardcream@kakao.com>
2024-11-22 02:52:38 +0900

    fix: fix playwright ci

--- apps/playground/playwright.config.ts
+++ apps/playground/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: process.env.CI ? "github" : "list",
   use: {
     baseURL: "http://localhost:5173",
     trace: "on-first-retry"
