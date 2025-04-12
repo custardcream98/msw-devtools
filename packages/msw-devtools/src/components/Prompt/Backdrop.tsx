3ce12cbd4259069274bfe0a55282161d3b8f087d
shiwoo.park <custardcream@kakao.com>
2025-04-13 00:33:19 +0900

    feat: add Prompt component

--- packages/msw-devtools/src/components/Prompt/Backdrop.tsx
+++ packages/msw-devtools/src/components/Prompt/Backdrop.tsx
@@ -1,3 +1,8 @@
 export const Backdrop = (props: React.ComponentPropsWithoutRef<"div">) => {
-  return <div className='fixed inset-0 bg-black bg-opacity-50' {...props} />
+  return (
+    <div
+      className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'
+      {...props}
+    />
+  )
 }
--- packages/msw-devtools/src/components/Prompt/Prompt.tsx
+++ packages/msw-devtools/src/components/Prompt/Prompt.tsx
@@ -1,4 +1,13 @@
 import { type JsonMock } from "core"
+import { jsonrepair } from "jsonrepair"
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+
+import { CodeEditor } from "~/components/CodeEditor"
+import { MethodPill } from "~/components/MethodPill"
+import { StatusPill } from "~/components/StatusPill"
+import { UrlText } from "~/components/UrlText"
+import { checkJSONParsable, isJSONFixable } from "~/lib/json"
 
 import { Backdrop } from "./Backdrop"
 
@@ -9,12 +18,48 @@ export const Prompt = ({
   jsonMock: JsonMock
   onSubmit: (response: string) => void
 }) => {
+  const [response, setResponse] = useState(
+    jsonMock.response.type === "single"
+      ? JSON.stringify(jsonMock.response.response, null, 2)
+      : JSON.stringify(jsonMock.response.response[0], null, 2)
+  )
+  const isFixable = isJSONFixable(response)
+  const isParsable = checkJSONParsable(response)
+
+  const { t } = useTranslation()
+
   return (
     <Backdrop>
       <div className='w-10/12 rounded-lg bg-white p-5'>
-        {JSON.stringify(jsonMock, null, 2)}
+        <div className='flex w-full items-center'>
+          <MethodPill className='mr-2' method={jsonMock.method} />
+          <StatusPill className='mr-2' status={jsonMock.status} />
+          <UrlText>{jsonMock.url}</UrlText>
+        </div>
+        <CodeEditor
+          className='mt-4 w-full'
+          value={response}
+          onChange={setResponse}
+        />
+        <button
+          type='submit'
+          className={
+            "button ml-auto mt-4 block w-fit flex-shrink-0 bg-green-600 text-xs text-white hover:bg-green-600 hover:text-white disabled:pointer-events-none disabled:bg-slate-400"
+          }
+          onClick={() => {
+            if (isFixable) {
+              setResponse(jsonrepair(response))
+              return
+            }
+            if (response) {
+              onSubmit(JSON.parse(response))
+            }
+          }}
+          disabled={!isParsable && !isFixable}
+        >
+          {isFixable ? t("prompt.fix") : t("prompt.submit")}
+        </button>
       </div>
-      <button type='button' onClick={() => onSubmit("test")}></button>
     </Backdrop>
   )
 }
