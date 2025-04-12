e8f35df69b03f9ac1a4ab60438a5aecc17122542
shiwoo.park <custardcream@kakao.com>
2025-04-13 03:19:50 +0900

    fix: handle multiple prompts

--- packages/msw-devtools/src/lib/msw/prompt.tsx
+++ packages/msw-devtools/src/lib/msw/prompt.tsx
@@ -1,38 +1,69 @@
 import { type Json, type JsonMock } from "core"
 import ReactDOMClient from "react-dom/client"
 
-let promptRoot: ReactDOMClient.Root | null = null
+let promptRootElement: HTMLDivElement | null = null
+const promptReactRoots = new Map<string, ReactDOMClient.Root>()
 
-const resetPromptRoot = () => {
-  if (promptRoot) {
-    promptRoot.unmount()
-    promptRoot = null
-  }
-}
-
-const createPromptRoot = () => {
+const createPromptRootElement = () => {
   const root = document.createElement("div")
   root.id = "msw-devtools-prompt"
   root.style.position = "fixed"
   root.style.zIndex = "999999"
+
   const devtoolsRoot = document.getElementById("msw-devtools")
   if (!devtoolsRoot) {
     throw new Error("[MSW Devtools] Devtools root not found")
   }
 
   devtoolsRoot.appendChild(root)
+
   return root
 }
 
-const getPromptRoot = () => {
-  if (!promptRoot) {
-    const root =
-      document.getElementById("msw-devtools-prompt") || createPromptRoot()
-
-    promptRoot = ReactDOMClient.createRoot(root)
+const getPromptRootElement = () => {
+  if (!promptRootElement) {
+    promptRootElement = createPromptRootElement()
   }
 
-  return promptRoot
+  return promptRootElement
+}
+
+const generatePromptId = () =>
+  `msw-devtools-prompt-${Date.now()}-${Math.random().toString(36)}`
+
+const resetPromptElement = (promptId: string) => {
+  const root = promptReactRoots.get(promptId)
+  if (root) {
+    root.unmount()
+    promptReactRoots.delete(promptId)
+    const promptElement = document.getElementById(promptId)
+    promptElement?.remove()
+  }
+}
+
+const createPromptElement = (promptId: string) => {
+  const root = document.createElement("div")
+  root.id = promptId
+  root.className = "hidden last:block"
+
+  const promptRootElement = getPromptRootElement()
+
+  promptRootElement.appendChild(root)
+
+  return root
+}
+
+const getPromptElement = (promptId: string) => {
+  let root = promptReactRoots.get(promptId)
+
+  if (!root) {
+    const element =
+      document.getElementById(promptId) || createPromptElement(promptId)
+    root = ReactDOMClient.createRoot(element)
+    promptReactRoots.set(promptId, root)
+  }
+
+  return root
 }
 
 export const createPrompt = (
@@ -42,14 +73,16 @@ export const createPrompt = (
     onSubmit: (response: Json) => void
   }) => React.ReactNode
 ) => {
+  const promptId = generatePromptId()
+
   return new Promise<Json>((resolve) => {
-    const root = getPromptRoot()
+    const root = getPromptElement(promptId)
     root.render(
       renderPrompt({
         jsonMock,
         onSubmit: (response) => {
           resolve(response)
-          resetPromptRoot()
+          resetPromptElement(promptId)
         }
       })
     )
