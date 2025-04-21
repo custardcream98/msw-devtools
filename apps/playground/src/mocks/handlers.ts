ce3011e047e21dcccd098f02309efd2dbe357cba
shiwoo.park <custardcream@kakao.com>
2025-04-21 13:41:41 +0900

    fix: playground default handler endpoint

--- apps/playground/src/mocks/handlers.ts
+++ apps/playground/src/mocks/handlers.ts
@@ -1,8 +1,10 @@
 import { http, HttpResponse } from "msw"
 
+import { ENDPOINT } from "../constants"
+
 export const handlers = [
   // Get all todos
-  http.get("/api/custard-status", () => {
+  http.get(ENDPOINT, () => {
     return HttpResponse.json("sweet")
   })
 ]
