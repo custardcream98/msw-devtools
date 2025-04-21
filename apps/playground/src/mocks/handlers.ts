a7f70c3114651efe14a42410b918a78d6974a124
shiwoo.park <custardcream@kakao.com>
2025-04-21 13:42:01 +0900

    chore: remove comment

--- apps/playground/src/mocks/handlers.ts
+++ apps/playground/src/mocks/handlers.ts
@@ -3,7 +3,6 @@ import { http, HttpResponse } from "msw"
 import { ENDPOINT } from "../constants"
 
 export const handlers = [
-  // Get all todos
   http.get(ENDPOINT, () => {
     return HttpResponse.json("sweet")
   })
