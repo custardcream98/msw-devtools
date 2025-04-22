6f7068f582b99a42c82fe506cc5577a95a8432b4
shiwoo.park <custardcream@kakao.com>
2025-04-23 07:37:14 +0900

    feat: improve playground

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -6,14 +6,13 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import App from "./App.tsx"
-import { handlers } from "./mocks/handlers.ts"
 
 const enableMocking = async () => {
   // if (import.meta.env.DEV) {
   // commented out for demo purposes
   return await installMSWDevtools({
     initialOpen: true,
-    setupWorker: setupWorker(...handlers),
+    setupWorker: setupWorker(),
     options: {
       onUnhandledRequest: "bypass"
     }
--- apps/playground/src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { http, HttpResponse } from "msw"
-
-import { ENDPOINT } from "../constants"
-
-export const handlers = [
-  http.get(ENDPOINT, () => {
-    return HttpResponse.json("sweet")
-  })
-]
